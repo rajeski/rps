c2061416ca86af4d007fd31c265580acc210d045
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-02-22 23:10:29 +0900

    Updated Project
    Continued adding code to this Python-file.

--- rps.py
+++ rps.py
@@ -22,7 +22,18 @@ while player == False:
             print('You win!', player, 'crushes' computer)
     elif player == 'Paper':
         if computer == 'Scissors':
-            print('You lose!', computer, 'cut', player)
+            print('You lose!', computer, 'cuts', player)
         else: 
             print('You win!', computer, 'covers', computer)
+    elif player == 'Scissors':
+        if computer == 'Rock':
+            print('You lose!', computer, 'crushes', player)
+        else:
+            print('You win!', player, 'cuts', computer) 
+    else:
+        print('That is not a valid option. Please check your spelling!')
+    #Player set to True, but to create a loop False allows it to
+    #continue
+    player = False
+    computer = t[randint(0,2)]  
     
